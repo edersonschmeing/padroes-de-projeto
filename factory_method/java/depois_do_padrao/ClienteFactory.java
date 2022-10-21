759ea159505ef03654e776ae38ca23ee925e84e3
edersonschmeing <edersonschmeing@gmail.com>
2022-10-21 10:50:11 -0300

    Factory Method - Implementado transporte Dutoviário

--- factory_method/java/depois_do_padrao/ClienteFactory.java
+++ factory_method/java/depois_do_padrao/ClienteFactory.java
@@ -2,6 +2,7 @@ package factory_method.java.depois_do_padrao;
 
 import factory_method.java.depois_do_padrao.modelo.Transporte;
 import factory_method.java.depois_do_padrao.factory.AereoFactory;
+import factory_method.java.depois_do_padrao.factory.DutoviarioFactory;
 import factory_method.java.depois_do_padrao.factory.FerroviarioFactory;
 import factory_method.java.depois_do_padrao.factory.HidroviarioFactory;
 import factory_method.java.depois_do_padrao.factory.RodoviarioFactory;
@@ -32,6 +33,13 @@ public class ClienteFactory {
         TransporteFactory rodoviarioFactory = new RodoviarioFactory();
         Transporte transporteRodoviario = rodoviarioFactory.ordemDeTransporte();
         System.out.println(transporteRodoviario.toString());
+
+
+        System.out.println("");
+        TransporteFactory dutoviarioFactory = new DutoviarioFactory();
+        Transporte transporteDutoviario = dutoviarioFactory.ordemDeTransporte();
+        System.out.println(transporteDutoviario.toString());
+
 
     }
 
