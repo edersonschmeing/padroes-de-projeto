7448ec4bb3351e7dace5b6a2368f9cadfbb73243
edersonschmeing <edersonschmeing@gmail.com>
2022-10-23 11:24:06 -0300

    Factory Method - Estrutura de diretórios

---
+++ factory_method/java/exemplo_transporte/antes_do_padrao/modelo/Hidroviario.java
@@ -0,0 +1,11 @@
+package factory_method.java.exemplo_transporte.antes_do_padrao.modelo;
+
+public class Hidroviario extends Transporte {
+
+    public void entregar() { 
+    
+        System.out.println("Entrega com transporte Hidroviário");
+
+    }  
+    
+}
