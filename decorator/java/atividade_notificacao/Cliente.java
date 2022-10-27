fdcaf74c168e6022ec845c25d13a7a5770974be2
edersonschmeing <edersonschmeing@gmail.com>
2022-10-26 21:55:44 -0300

    Decorator

---
+++ decorator/java/atividade_notificacao/Cliente.java
@@ -0,0 +1,11 @@
+package decorator.java.atividade_notificacao;
+
+public class Cliente {
+
+    public static void main(String[] args) {
+
+       System.out.println("### teste o desafio do Decorator ###");
+
+    }
+    
+}
