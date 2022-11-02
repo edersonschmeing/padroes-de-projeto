421437ec02dee5b974cdb573f3836093f3c6e6cf
edersonschmeing <edersonschmeing@gmail.com>
2022-11-02 12:49:06 -0300

    Facade - exemplo_conversao-video

---
+++ facade/java/exemplo_conversao_video/Cliente.java
@@ -0,0 +1,17 @@
+package facade.java.exemplo_conversao_video;
+
+import facade.java.exemplo_conversao_video.facade.ConversaoDeVideoFacade;
+
+import java.io.File;
+
+public class Cliente {
+
+    public static void main(String[] args) {
+        
+        ConversaoDeVideoFacade converter = new ConversaoDeVideoFacade();
+        
+        File video_mp4 = converter.converterVideo("youtubevideo.ogg", "mp4");
+        // ...
+    }
+    
+}
