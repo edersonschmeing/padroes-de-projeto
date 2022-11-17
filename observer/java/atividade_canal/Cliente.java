4e90636132a5ccb26ccd1360001076fe3c1815d9
edersonschmeing <edersonschmeing@gmail.com>
2022-11-17 18:42:17 -0300

    Observer - diagrama

--- observer/java/atividade_canal/Cliente.java
+++ observer/java/atividade_canal/Cliente.java
@@ -3,17 +3,6 @@ package observer.java.atividade_canal;
 public class Cliente {
     
      //implemente
-     
-     /* 
-      Criar no mínimo quatro pessoas que devem assinar um canal e 
-      toda vez que for adicionado novos conteúdos nesse canal as 
-      pessoas deveram receber notificões. 
-
-      Utilize como base o diagrama, template, exemplo das temperaturas e 
-      orientações dadas pelo professor na aula. 
-      
-      */
-
-    
+         
 
 }
